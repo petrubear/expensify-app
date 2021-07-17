4c5c91a9d441ef5df3f37ffccfd6c5d5fdd40cd1
petrubear <edison.martinez@gmail.com>
2021-07-16 19:24:41 -0500

    feat: add 404

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -30,14 +30,21 @@ const HelpPage = () => (
     </div>
 );
 
+const NotFoundPage = () => (
+    <div>
+        404!
+    </div>
+);
+
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact={true}/>
             <Route path="/create" component={AddExpensePage}/>
             <Route path="/edit" component={EditExpensePage}/>
             <Route path="/help" component={HelpPage}/>
-        </div>
+            <Route component={NotFoundPage}/>
+        </Switch>
     </BrowserRouter>
 );
 
