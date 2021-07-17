9af4df477c37651db4e26e4a2455bd0866496c9a
petrubear <edison.martinez@gmail.com>
2021-07-17 17:50:55 -0500

    feat: refactor componentes en archivos independientes

--- src/app.js
+++ src/app.js
@@ -2,64 +2,9 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        This is my dashboard component
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        This is my add component
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        This is my edit component
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        This is my help component
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404! - <Link to="/">Go Home</Link>
-    </div>
-);
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </header>
-);
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Header/>
-            <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-                <Route path="/create" component={AddExpensePage}/>
-                <Route path="/edit" component={EditExpensePage}/>
-                <Route path="/help" component={HelpPage}/>
-                <Route component={NotFoundPage}/>
-            </Switch>
-        </div>
-    </BrowserRouter>
-);
-
 const appRoot = document.getElementById('app');
-ReactDOM.render(routes, appRoot);
+ReactDOM.render(<AppRouter />, appRoot);
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+    <div>
+        This is my edit component
+    </div>
+);
+export default EditExpensePage;
