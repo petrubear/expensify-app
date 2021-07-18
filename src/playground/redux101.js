45491cf0433f1bb1eebae0b890343afae3a9979d
petrubear <edison.martinez@gmail.com>
2021-07-18 17:50:02 -0500

    feat: redux paramenters

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -3,38 +3,56 @@ import {createStore} from 'redux';
 const store = createStore((state = {count: 0}, action) => {
     switch (action.type) {
     case 'INCREMENT':
+        const incrementBy =
+            typeof action.incrementBy === 'number' ? action.incrementBy : 1;
         return {
-            count: state.count + 1
+            count: state.count + incrementBy,
         };
     case 'DECREMENT':
+        const decrementBy =
+            typeof action.decrementBy === 'number' ? action.decrementBy : 1;
         return {
-            count: state.count -1
+            count: state.count - decrementBy,
         };
     case 'RESET':
         return {
-            count: 0
+            count: 0,
+        };
+    case 'SET':
+        return {
+            count: action.count,
         };
     default:
         return state;
     }
 });
 
-console.log(store.getState());
-
-store.dispatch({
-    type: 'INCREMENT',
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
 });
 
 
 store.dispatch({
     type: 'INCREMENT',
+    incrementBy: 5,
 });
+
+// unsubscribe();
+
+store.dispatch({
+    type: 'INCREMENT',
+});
+
 store.dispatch({
     type: 'RESET',
 });
 
 store.dispatch({
     type: 'DECREMENT',
+    decrementBy: 10,
 });
 
-console.log(store.getState());
+store.dispatch({
+    type: 'SET',
+    count: 101,
+});
