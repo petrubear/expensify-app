a110aa34430cc82dfef3e078e30866f935481d09
petrubear <edison.martinez@gmail.com>
2021-07-19 17:59:29 -0500

    feat: reducer as function

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -27,7 +27,10 @@ const setCount = ({count}) => {
     };
 };
 
-const store = createStore((state = {count: 0}, action) => {
+// reducer
+// 1 reducers are pure functions
+// 2 never change state or action
+const countReducer = (state = {count: 0}, action) => {
     switch (action.type) {
     case 'INCREMENT':
         return {
@@ -50,7 +53,9 @@ const store = createStore((state = {count: 0}, action) => {
     default:
         return state;
     }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
