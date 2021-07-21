d155e46012e753f2fd84b6beef74a95a3e45c7d9
petrubear <edison.martinez@gmail.com>
2021-07-20 19:38:44 -0500

    feat: add react-redux, connect components

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {addExpense} from './actions/expenses';
@@ -29,5 +30,11 @@ const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
+const jsx = (
+    <Provider store={store}>
+        <AppRouter/>
+    </Provider>
+);
+
 const appRoot = document.getElementById('app');
-ReactDOM.render(<AppRouter/>, appRoot);
+ReactDOM.render(jsx, appRoot);
