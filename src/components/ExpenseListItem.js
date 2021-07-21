971d40a1a684e9b7675d0b3e6e87073fe6c51994
petrubear <edison.martinez@gmail.com>
2021-07-20 20:20:25 -0500

    feat: dispatch actions from components

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,17 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import {connect} from 'react-redux';
+import {removeExpense} from '../actions/expenses';
 
-const ExpenseListItem = ({description, amount, createdAt}) => (
+const ExpenseListItem = ({id, description, amount, createdAt, dispatch}) => (
     <div>
         <h3>{description}</h3>
         <p>{amount} - {createdAt}</p>
+        <button onClick={() => {
+            dispatch(removeExpense({id}));
+        }}>Remove
+        </button>
     </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
