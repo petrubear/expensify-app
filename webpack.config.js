6e59df416c8419597ef58525f7caaf14688c350a
petrubear <edison.martinez@gmail.com>
2021-07-20 20:27:23 -0500

    chore: dev source maps

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,8 @@ module.exports = {
             ],
         }],
     },
-    devtool: 'eval-cheap-module-source-map',
+    devtool: 'eval-source-map',
+    // devtool: 'eval-cheap-module-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true,
