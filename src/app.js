9447536e9b4a72b48d8b1658420c772e4bba3a89
petrubear <edison.martinez@gmail.com>
2021-07-21 21:04:09 -0500

    feat: edit expense

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
-/* eslint-disable require-jsdoc */
-/* eslint-disable react/prop-types */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {addExpense} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
@@ -17,13 +14,6 @@ const store = configureStore();
 store.dispatch(addExpense({description: 'Water Bill', amount: 40.0}));
 store.dispatch(addExpense({description: 'Gas Bill', amount: 70.0, createdAt: 1000}));
 store.dispatch(addExpense({description: 'Rent', amount: 800.0}));
-
-/*
-store.dispatch(setTextFilter('bill'));
-setTimeout(() => {
-    store.dispatch(setTextFilter('water'));
-}, 3000);
-*/
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
