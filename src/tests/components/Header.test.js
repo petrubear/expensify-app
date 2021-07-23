666fcd147c150e2fd25c2c3d39dad3f118b3aad3
petrubear <edison.martinez@gmail.com>
2021-07-22 19:52:57 -0500

    feat: add react-test-renderer

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+import Header from '../../components/Header';
+
+test('should render Header', () => {
+    const renderer = new ReactShallowRenderer();
+    renderer.render(<Header/>);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
