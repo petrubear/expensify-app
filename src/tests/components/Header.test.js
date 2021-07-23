2895b8cb67901cae88cd9d170b15ab2c9afbaed2
petrubear <edison.martinez@gmail.com>
2021-07-22 20:22:06 -0500

    feat: add enzyme for snapshot testing

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import {shallow} from 'enzyme';
 import Header from '../../components/Header';
 
+
 test('should render Header', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header/>);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header/>);
+    expect(wrapper).toMatchSnapshot();
 });
