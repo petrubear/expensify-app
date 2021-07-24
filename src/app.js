7a309a0642db9a04dab2f8f7a4156cbac2aeb305
petrubear <edison.martinez@gmail.com>
2021-07-24 18:28:45 -0500

    feat: production webpack build

--- src/app.js
+++ src/app.js
@@ -3,23 +3,22 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import {addExpense} from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
+// import {addExpense} from './actions/expenses';
+// import getVisibleExpenses from './selectors/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
+/*
+const state = store.getState();
 store.dispatch(addExpense({description: 'Water Bill', amount: 40.0}));
 store.dispatch(addExpense({description: 'Gas Bill', amount: 70.0, createdAt: 1000}));
 store.dispatch(addExpense({description: 'Rent', amount: 800.0}));
-
-const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
-
+*/
 const jsx = (
     <Provider store={store}>
         <AppRouter/>
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,35 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    // entry: './src/playground/hoc.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/,
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader',
-            ],
-        }],
-    },
-    devtool: 'eval-source-map',
-    // devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true,
-    },
+module.exports = (env) => {
+    console.log('env', env);
+    const isProduction = !!env.production;
+    console.log(`isProduction: ${isProduction}`);
+    return {
+        mode: isProduction ? 'production' : 'development',
+        entry: './src/app.js',
+        // entry: './src/playground/hoc.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }],
+        },
+        devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true,
+        },
+    };
 };
