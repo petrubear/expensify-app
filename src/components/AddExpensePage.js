72887ca20a5ae82745c9df587cf21ed2e0273360
petrubear <edison.martinez@gmail.com>
2021-07-23 20:18:19 -0500

    feat: add / edit expenses test

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,16 +3,32 @@ import ExpenseForm from './ExpenseForm';
 import {connect} from 'react-redux';
 import {addExpense} from '../actions/expenses';
 
-const AddExpensePage = (props) => (
-    <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm
-            onSubmit={(expense) => {
-                props.dispatch(addExpense(expense));
-                props.history.push('/');
-            }}
-        />
-    </div>
-);
+export class AddExpensePage extends React.Component {
+    onSubmit = (expense) => {
+        this.props.addExpense(expense);
+        this.props.history.push('/');
+    };
 
-export default connect()(AddExpensePage);
+    render() {
+        return (
+            <div>
+                <h1>Add Expense</h1>
+                <ExpenseForm
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        );
+    }
+}
+
+// utilizo esto para facilitar las pruebas unitarias
+// y esto me obliga a comvertirlo en clase
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addExpense: (expense) => {
+            dispatch(addExpense(expense));
+        },
+    };
+};
+
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
