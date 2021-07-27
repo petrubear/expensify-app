fab0443a0a2241178765a3edc2cfd5adea4debb6
petrubear <edison.martinez@gmail.com>
2021-07-27 18:46:49 -0500

    feat: add logout button

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,18 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import {shallow} from 'enzyme';
-import Header from '../../components/Header';
+import {Header} from '../../components/Header';
 
 
 test('should render Header', () => {
-    const wrapper = shallow(<Header/>);
+    const wrapper = shallow(<Header startLogout={() => {
+    }}/>);
     expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogout on click', () => {
+    const startLogoutSpy = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogoutSpy}/>);
+    wrapper.find('button').simulate('click');
+    expect(startLogoutSpy).toHaveBeenCalled();
 });
--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -8,3 +8,10 @@ test('should render LoginPage', () => {
     const wrapper = shallow(<LoginPage/>);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should call startLogin on click', () => {
+    const startLoginSpy = jest.fn();
+    const wrapper = shallow(<LoginPage startLogin={startLoginSpy}/>);
+    wrapper.find('button').simulate('click');
+    expect(startLoginSpy).toHaveBeenCalled();
+});
