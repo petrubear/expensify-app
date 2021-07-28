f172f753aa7950ed48c83c717d9e7fb412ee7f89
petrubear <edison.martinez@gmail.com>
2021-07-27 19:41:47 -0500

    feat: add auth reducer

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, {history} from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {startSetExpenses} from './actions/expenses';
+import {login, logout} from './actions/auth';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -27,15 +28,29 @@ const jsx = (
 );
 
 const appRoot = document.getElementById('app');
+
+let hasRendered = false;
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(jsx, appRoot);
+        hasRendered = true;
+    }
+};
+
 ReactDOM.render(<p>Loading...</p>, appRoot);
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, appRoot);
-});
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-        console.log('current user is authenticated');
+        store.dispatch(login(user.uid));
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if (history.location.pathname === '/') {
+                history.push('/dashboard');
+            }
+        });
     } else {
-        console.log('current user is not authenticated');
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
     }
 });
